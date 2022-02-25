0f82341cd27d3c20a6d16d6d51cff505b0ebd51b
Gordo <gordonhdo@gmail.com>
2022-02-24 20:25:23 -0800

    Updated index.js added init function to generate readme

--- index.js
+++ index.js
@@ -2,71 +2,86 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util')
-const markdown = require('./utils/generateMarkdown');
+const writeToFile = util.promisify(fs.writeFile)
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// Prompt user with inquirer for Readme.md content
-// Project Title, Description, Usage, Installation, Contributing, Tests, Questions, License
-const userPrompt = [
+// Function prompts user using inquirer.prompt to Readme.md Content
+// Generate questions for: Project Title, Description, Usage, Installation, Contributing, Tests, Questions, License
+// Table of Contents generated in generateMarkdown file)
+function userPrompt(){
+    return inquirer.prompt ([
     // Prompt user for project title
-    {
-        name: "projectTitle",
-        message: "What would you like your project to be called?",
-        type: "input",
-
-    },
-    // Prompt user for short project description
-    {
-        name: "description",
-        message: "Briefly describe your project:",
-        type: "input",
-    },
-    // Pompt user for usage instructions
-    {
-        name: "usage",
-        message: "Leave a written demonstration on how to use your project",
-        type: "input",
-    },
-    // Prompt user for installation instructions
-    {
-        name: "installation",
-        message: "In this section, provide installation instructions for your project if required",
-        type: "input",
-    },
-    // Prompt user for project contributors
-    {
-        name: "contributing",
-        message: "List your contributors for this project",
-        type: "input",
-    },
-    {
-        name: "test",
-        message: "Provide test information for your project (if applicable)",
-        type: "input",
-    },
-    {
-        name: "questions",
-        message: "Section for further questions regarding your project",
-        type: "input",
-    },
-    
-
-];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, err => {
-        if(err) {
-            return console.log(err)
-        }
-    });
+        {
+            name: "projectTitle",
+            message: "What is the name of your project?: ",
+            type: "input",
+        },
+        // Prompt user for short project description
+        {
+            name: "description",
+            message: "Briefly describe your project: ",
+            type: "input",
+        },
+        // Pompt user for usage instructions
+        {
+            name: "usage",
+            message: "Enter your project usage information: ",
+            type: "input",
+        },
+        // Prompt user for installation instructions
+        {
+            name: "installation",
+            message: "If required, provide installation instructions for your project: ",
+            type: "input",
+        },
+        // Prompt user for project contributors
+        {
+            name: "contributing",
+            message: "Display contribution guidelines here: ",
+            type: "input",
+        },
+        {
+            name: "test",
+            message: "Provide test information or procedures for your project: ",
+            type: "input",
+        },
+        {
+            name: "questions",
+            message: "Please provide GitHub profile for questions section: ",
+            type: "input",
+        },
+    ]);
 }
 
+// Function writes README.md file using fs module
+// function writeToFile(fileName, data) {
+//     fs.writeFile(fileName, data, err => {
+//         if(err) {
+//             return console.log(err)
+//         }
+//         console.log("README.md has been created")
+//     });
+// }
+
 // TODO: Create a function to initialize app
+// Combined function using util.promisify(fs.writeFile) initializes app then creates Readme
 async function init() {
     try {
-
+        // function initializes after user answers prompt
+        const userInput = await userPrompt();
+        // generate readme using markdown after user inputs
+        const generateReadme = generateMarkdown(userInput);
+        // Create Readme.md file in newReadme
+        await writeToFile('./newReadMe/Readme.md', generateReadme);
+        console.log('Your README.md has been created');
+    } catch (err) {
+        console.log(err)
     }
 }
 
 // Function call to initialize app
 init();
+
+
+// External Sources:
+// https://stackoverflow.com/questions/67797574/using-async-await-with-util-promisifyfs-readfile
